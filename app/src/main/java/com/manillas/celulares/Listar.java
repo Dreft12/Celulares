52e71a5e570ba5a56174f2d18f9fe528c9038d19
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-13 19:53:20 -0500

    V 0.4 Añadiendo Listar
    Se añade listar para mostrar los telefonos que están en la lista.

---
+++ app/src/main/java/com/manillas/celulares/Listar.java
@@ -0,0 +1,49 @@
+package com.manillas.celulares;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Listar extends AppCompatActivity {
+    private TableLayout tabla;
+    private LinkedList<Celular> celulares;
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar);
+        tabla = findViewById(R.id.tabla);
+        personas = Datos.obtener();
+        for (int i = 0; i < celulares.size(); i++){
+            TableRow row = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+            TextView c4 = new TextView(this);
+            TextView c5 = new TextView(this);
+            TextView c6 = new TextView(this);
+            TextView c7 = new TextView(this);
+            c1.setText(String.valueOf(i+1));
+            c2.setText(celulares.get(i).getMarca());
+            c3.setText(celulares.get(i).getModelo());
+            c4.setText(celulares.get(i).getColor());
+            c5.setText(celulares.get(i).getAndroid());
+            c6.setText(String.format("%.1f",celulares.get(i).getRam()));
+            c7.setText(celulares.get(i).getPrecio());
+            row.addView(c1);
+            row.addView(c2);
+            row.addView(c3);
+            row.addView(c4);
+            row.addView(c5);
+            row.addView(c6);
+            row.addView(c7);
+            tabla.addView(row);
+        }
+    }
+}
