dcdd7dce458bac30d39523703ca7b97e3f58e04b
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-09-13 21:13:18 -0500

    V 1.1 Reporte añadido
    Añadiendo reportes numero 2 y cuatro a los metodos y a la vista

--- app/src/main/java/com/manillas/celulares/Metodos.java
+++ app/src/main/java/com/manillas/celulares/Metodos.java
@@ -3,15 +3,39 @@ package com.manillas.celulares;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Metodos implements Serializable{
+public class Metodos implements Serializable {
     private static ArrayList<Celular> lista = Datos.mostrar();
-    public static ArrayList<Celular> reporteUno(){
+
+    public static ArrayList<Celular> reporteUno() {
         ArrayList<Celular> temp = new ArrayList<>();
-        for (int i = 0; i< lista.size(); i++){
-            if (lista.get(i).getMarca().equalsIgnoreCase("Samsung") && lista.get(i).getColor().equalsIgnoreCase("Negro")){
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getMarca().equalsIgnoreCase("Samsung") && lista.get(i).getColor().equalsIgnoreCase("Negro")) {
                 temp.add(lista.get(i));
             }
         }
         return temp;
     }
+
+    public static ArrayList<Celular> reporteDos() {
+        ArrayList<Celular> temp = new ArrayList<>();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getMarca().equalsIgnoreCase("Samsung")) {
+                if (lista.get(i).getRam()>=2 && lista.get(i).getRam() <= 4){
+                    temp.add(lista.get(i));
+                }
+            }
+        }
+        return temp;
+    }
+
+    public static int reporteCuatro(){
+        int cantidad = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getMarca().equalsIgnoreCase("Apple") && lista.get(i).getColor().equalsIgnoreCase("Negro")){
+                cantidad++;
+            }
+        }
+        return cantidad;
+    }
 }
+
--- app/src/main/java/com/manillas/celulares/Reportes.java
+++ app/src/main/java/com/manillas/celulares/Reportes.java
@@ -1,5 +1,7 @@
 package com.manillas.celulares;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -11,11 +13,19 @@ import android.widget.Spinner;
 public class Reportes extends AppCompatActivity {
     private Spinner cmbRepor;
     private Intent in;
+    private AlertDialog.Builder mensaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reportes);
         cmbRepor = (Spinner) findViewById(R.id.cmbRepor);
+        mensaje = new AlertDialog.Builder(this);
+        mensaje.setTitle(getResources().getString(R.string.reporCuatro));
+        mensaje.setPositiveButton(getResources().getText(R.string.ok), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface mensaje, int id) {
+
+            }
+        });
     }
 
     public void mostrar(View v){
@@ -26,7 +36,17 @@ public class Reportes extends AppCompatActivity {
                 startActivity(in);
                 break;
             }
+            case 2:{
+                in = new Intent(this,ListarReportes.class);
+                in.putExtra("reporte", Metodos.reporteDos());
+                startActivity(in);
+                break;
+            }
+            case 4:{
+                mensaje.setMessage(getResources().getString(R.string.reporCuatroMensaje) + Metodos.reporteCuatro());
+                mensaje.show();
+                break;
+            }
         }
     }
-
 }
